b98f7e51fbeb5c386ed4ba351fc6ce8b41d91804
Joerg <joerg.hoffmann@hsr.ch>
2018-12-06 05:20:08 +0100

    Win8:Login:define Setup

---
+++ Win8/login/StartAndLogin/StartAndLogin/Program.vb
@@ -0,0 +1,48 @@
+﻿'
+' Created by Ranorex
+' User: Jörg
+' Date: 06.12.2018
+' Time: 05:05
+' 
+' To change this template use Tools > Options > Coding > Edit standard headers.
+'
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Threading
+Imports System.Drawing
+Imports System.Collections.Generic
+Imports System.Text.RegularExpressions
+Imports WinForms = System.Windows.Forms
+
+Imports Ranorex
+Imports Ranorex.Core
+Imports Ranorex.Core.Reporting
+Imports Ranorex.Core.Testing
+
+Namespace StartAndLogin
+Module Program
+
+    <STAThread()> _
+    Function Main() As Integer
+        ' Uncomment the following 3 lines if you want to automate Windows apps
+        ' by starting the test executable directly
+        'If Util.IsRestartRequiredForWinAppAccess Then
+        '    Return Util.RestartWithUiAccess()
+        'End If
+
+        Keyboard.AbortKey = System.Windows.Forms.Keys.Pause
+        Dim errorNumber As Integer = 0
+
+        Try
+            errorNumber = TestSuiteRunner.Run(GetType(Program), Environment.CommandLine)
+        Catch e As Exception
+            Report.Error("Unexpected exception occurred: " + e.ToString())
+            errorNumber = -1
+        End Try
+
+        Return errorNumber
+
+    End Function
+End Module
+End Namespace
---
+++ Win8/login/StartAndLogin/StartAndLogin/UserCodeModule1.vb
@@ -0,0 +1,49 @@
+﻿'
+' Created by Ranorex
+' User: Jörg
+' Date: 06.12.2018
+' Time: 05:09
+' 
+' To change this template use Tools > Options > Coding > Edit standard headers.
+'
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Drawing
+Imports System.Threading
+Imports WinForms = System.Windows.Forms
+
+Imports Ranorex
+Imports Ranorex.Core
+Imports Ranorex.Core.Testing
+
+Namespace StartAndLogin
+   ''' <summary>
+   ''' Description of UserCodeModule1.
+   ''' </summary>
+   <TestModule("88F248A7-90BA-4983-A046-C9E89ABFAC75", ModuleType.UserCode, 1)> _
+   Public Class UserCodeModule1
+        Implements ITestModule
+
+        ''' <summary>
+        ''' Constructs a new instance.
+        ''' </summary>
+        Public Sub New()
+            ' Do not delete - a parameterless constructor is required!
+        End Sub
+
+        ''' <summary>
+        ''' Performs the playback of actions in this module.
+        ''' </summary>
+        ''' <remarks>You should not call this method directly, instead pass the module
+        ''' instance to the <see cref="TestModuleRunner.Run(Of ITestModule)"/> method
+        ''' that will in turn invoke this method.</remarks>
+        Sub Run() Implements ITestModule.Run
+            Mouse.DefaultMoveTime = 300
+            Keyboard.DefaultKeyPressTime = 100
+            Delay.SpeedFactor = 1.0
+        End Sub
+
+    End Class
+End Namespace
